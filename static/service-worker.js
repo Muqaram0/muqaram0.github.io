6817cbe7f5b736a0c1098b3b62ba8d69ee46a18d
Muqaram Majid <muqaram806@gmail.com>
2026-04-15 17:53:19 +0400

    Add Pages cache cleanup and nojekyll marker

---
+++ static/service-worker.js
@@ -0,0 +1,15 @@
+﻿self.addEventListener('install', function () {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil((async function () {
+    try {
+      const keys = await caches.keys();
+      await Promise.all(keys.map(function (k) { return caches.delete(k); }));
+    } catch (e) {}
+    await self.registration.unregister();
+    const clientsList = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    clientsList.forEach(function (client) { client.navigate(client.url); });
+  })());
+});
